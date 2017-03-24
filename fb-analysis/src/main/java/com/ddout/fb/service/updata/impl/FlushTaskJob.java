d5ccc80c5d7ee2823418e2ee29f1f8931ec822f8
ddout <ddout@qq.com>
2017-03-24 18:57:07 +0800

    job 14

--- fb-analysis/src/main/java/com/ddout/fb/service/updata/impl/FlushTaskJob.java
+++ fb-analysis/src/main/java/com/ddout/fb/service/updata/impl/FlushTaskJob.java
@@ -4,6 +4,7 @@ import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.cdhy.commons.utils.SysConfigUtil;
+import com.ddout.fb.service.parse.ICrawlerService;
 import com.ddout.fb.service.updata.IUpDataService;
 
 public class FlushTaskJob {
@@ -12,6 +13,8 @@ public class FlushTaskJob {
     private static final String IS_LOAD_FLUSH = SysConfigUtil.getInstance().getProperites("is_load_flush");
     @Autowired
     private IUpDataService service;
+    @Autowired
+    private ICrawlerService crawlerService;
 
     public void exec() {
 	log.debug("BEGIN------------- FlushTaskJob");
@@ -30,6 +33,11 @@ public class FlushTaskJob {
 	}
 	try {
 	    service.updateOldMatch();
+	} catch (Exception e) {
+	    log.error(e);
+	}
+	try {
+	    crawlerService.parseZucai14();
 	} catch (Exception e) {
 	    log.error(e);
 	}
